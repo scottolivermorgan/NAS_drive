b97857d69e48c1f290d9667559bf709e4b3f8d2a
Scott Oliver Morgan <scottolivermorgan@yahoo.com>
2023-07-16 13:03:56 +0100

    Added Nexcloud installation script.

---
+++ nextcloud-installation.sh
@@ -0,0 +1,23 @@
+# Change to default Apache2 directory
+cd /var/www
+
+# Download Nextcloud
+wget https://download.nextcloud.com/server/releases/latest.tar.bz2
+
+# Extract the archive
+tar -xvf latest.tar.bz2
+
+# Make directory for Nextcloud to operate in
+mkdir -p /var/www/nextcloud/data
+
+# Update permissions
+chown -R www-data:www-data /var/www/nextcloud/
+
+#Move Nextcloud config file to correct location for Apache
+mv /home/pi/NAS_drive/nextcloud.conf /etc/apache2/sites-available/
+
+# Point Apahe2 to config file for Nextcloud
+a2ensite nextcloud.conf
+
+# Restart Apache2 to load ne confi
+systemctl reload apache2
