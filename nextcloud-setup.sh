7cbb80363f86490d7e94c2d01e594f07c6e50fe0
Scott Oliver Morgan <scottolivermorgan@yahoo.com>
2023-07-16 11:49:53 +0100

    Pdate db creation.

--- nextcloud-setup.sh
+++ nextcloud-setup.sh
@@ -1,8 +1,7 @@
 # Create & configure database for nextcloud
 
-sudo mysql -u root -p
-CREATE DATABASE nextclouddb;
-CREATE USER 'nextclouduser'@'localhost' IDENTIFIED BY 'shieldsDown';
-GRANT ALL PRIVILEGES ON nextclouddb.* TO 'nextclouduser'@'localhost';
-FLUSH PRIVILEGES;
-Quit;
+echo "CREATE DATABASE nextclouddb;
+      CREATE USER 'nextclouduser'@'localhost' IDENTIFIED BY 'shieldsDown';
+      GRANT ALL PRIVILEGES ON nextclouddb.* TO 'nextclouduser'@'localhost';
+      FLUSH PRIVILEGES;
+      Quit;" | sudo mysql -u root -p
