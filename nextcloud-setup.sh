3cad0a43b54ff49fbc03bf58b512e0bd62032e70
Scott Oliver Morgan <scottolivermorgan@yahoo.com>
2023-07-16 12:06:55 +0100

    Syntax error in SQL.

--- nextcloud-setup.sh
+++ nextcloud-setup.sh
@@ -4,4 +4,4 @@ echo "CREATE DATABASE nextclouddb;
       CREATE USER 'nextclouduser'@'localhost' IDENTIFIED BY 'shieldsDown';
       GRANT ALL PRIVILEGES ON nextclouddb.* TO 'nextclouduser'@'localhost';
       FLUSH PRIVILEGES;
-      Quit;" | sudo mysql -u root -p
+      Quit" | sudo mysql -u root -p
