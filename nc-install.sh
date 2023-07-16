53ad9ba9349f57ac510f450234be594e78d68192
Scott Oliver Morgan <scottolivermorgan@yahoo.com>
2023-07-16 19:48:34 +0100

    yes tags

--- nc-install.sh
+++ nc-install.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 #remove unused packages and clean cache
-sudo apt autoremove
-sudo apt clean
+sudo apt autoremove -y
+sudo apt clean -y
 
 # Install apache
-apt install apache2
+apt install apache2 -y
 
 # Add third party PHP repo
-curl https://packages.sury.org/php/apt.gpg | sudo tee /usr/share/keyrings/suryphp-archive-keyring.gpg >/dev/null
-echo "deb [signed-by=/usr/share/keyrings/suryphp-archive-keyring.gpg] https://packages.sury.org/php/ $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/sury-php.list
-apt update
+curl https://packages.sury.org/php/apt.gpg | sudo tee /usr/share/keyrings/suryphp-archive-keyring.gpg >/dev/null -y
+echo "deb [signed-by=/usr/share/keyrings/suryphp-archive-keyring.gpg] https://packages.sury.org/php/ $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/sury-php.list -y
+apt update -y
 
 # Install relevent PHP packages
-apt install php8.1 php8.1-gd php8.1-sqlite3 php8.1-curl php8.1-zip php8.1-xml php8.1-mbstring php8.1-mysql php8.1-bz2 php8.1-intl php8.1-smbclient php8.1-imap php8.1-gmp php8.1-bcmath libapache2-mod-php8.1
+apt install php8.1 php8.1-gd php8.1-sqlite3 php8.1-curl php8.1-zip php8.1-xml php8.1-mbstring php8.1-mysql php8.1-bz2 php8.1-intl php8.1-smbclient php8.1-imap php8.1-gmp php8.1-bcmath libapache2-mod-php8.1 -y
 
 # Install mariaDB
-apt install mariadb-server
+apt install mariadb-server -y
 
 # Create & configure database for nextcloud
 
@@ -33,7 +33,7 @@ cd /var/www
 wget https://download.nextcloud.com/server/releases/latest.tar.bz2
 
 # Extract the archive
-tar -xvf latest.tar.bz2
+tar -xvf latest.tar.bz2 -y
 
 # Make directory for Nextcloud to operate in
 mkdir -p /var/www/nextcloud/data
