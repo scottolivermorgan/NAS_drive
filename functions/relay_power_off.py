78f26f67f900574238395bcabf33d1f016a5223b
Scott Oliver Morgan <scottolivermorgan@yahoo.com>
2023-08-12 10:47:10 +0100

    Tidied dir structure.

---
+++ functions/relay_power_off.py
@@ -0,0 +1,11 @@
+import RPi.GPIO as GPIO
+
+def power_off():
+    GPIO.output(RELAY_CHANNEL, GPIO.LOW)
+
+if __name__ == "__main__":
+    RELAY_CHANNEL = 26
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(RELAY_CHANNEL, GPIO.OUT)
+    power_off()
---
+++ functions/relay_power_on.py
@@ -0,0 +1,11 @@
+import RPi.GPIO as GPIO
+
+def power_on():
+    GPIO.output(RELAY_CHANNEL, GPIO.HIGH)
+
+if __name__ == "__main__":
+    RELAY_CHANNEL = 26
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(RELAY_CHANNEL, GPIO.OUT)
+    power_on()
