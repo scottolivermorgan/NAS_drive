3001c03c89fb3d318d398b7151ad6dce29513d31
Scott Oliver Morgan <scottolivermorgan@yahoo.com>
2023-08-12 13:34:35 +0100

    cron command error fixed.

--- scripts/backup_drive/schedule-backup.sh
+++ scripts/backup_drive/schedule-backup.sh
@@ -1,4 +1,4 @@
 # Write scheduled job to cron.
-(crontab -l ; echo "* * * * * /home/pi/NAS_drive/scripts/backup_drive/schedule-backup.sh")| crontab -
+(crontab -l ; echo "* * * * * /home/pi/NAS_drive/functions/back-up.sh")| crontab -
 
 #echo "$(echo '* * * * * /NAS_drive/functions/back-up.sh' ; ssh pi@raspberrypi crontab -l 2>&1)" | ssh pi@raspberrypi "crontab -"
