9a1f0b911e8e210e87318ef6e2e4f25c186e4904
Scott Oliver Morgan <scottolivermorgan@yahoo.com>
2023-08-12 13:14:52 +0100

    cron test.

--- scripts/backup_drive/schedule-backup.sh
+++ scripts/backup_drive/schedule-backup.sh
@@ -1,2 +1,4 @@
 # Write scheduled job to cron.
-(crontab -l ; echo "* * * * * /NAS_drive/functions/back-up.sh")| crontab -
+#(crontab -l ; echo "* * * * * /NAS_drive/functions/back-up.sh")| crontab -
+
+echo "$(echo '* * * * * /NAS_drive/functions/back-up.sh' ; ssh pi@raspberrypi crontab -l 2>&1)" | ssh pi@raspberrypi "crontab -"
