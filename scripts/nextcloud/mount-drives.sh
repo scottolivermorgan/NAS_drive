835bcb2882d00ffcc452186ab27be07378687bdb
Scott Oliver Morgan <scottolivermorgan@yahoo.com>
2023-08-30 19:33:34 +0100

    spelling

--- scripts/nextcloud/mount-drives.sh
+++ scripts/nextcloud/mount-drives.sh
@@ -1,5 +1,5 @@
 # Harddrive location
-mkdir /media/harddrive1
+mkdir /media/hardrive1
 
 # Mount harddrive on boot
 #echo "/dev/sda2 /media/hardrive1    auto    uid=1000,gid=1000,noatime 0 0" >> /etc/fstab
