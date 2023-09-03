5810f99e53b17412685466fe6e2ec50e370b4f5f
Scott Oliver Morgan <scottolivermorgan@yahoo.com>
2023-09-03 13:49:33 +0100

    5min cron test.

--- scripts/backup_drive/schedule-backup.sh
+++ scripts/backup_drive/schedule-backup.sh
@@ -1,2 +1,4 @@
 # Write scheduled job to cron.
-(crontab -l ; echo "* * * * * sh /home/scott/NAS_drive/functions/back-up.sh")| crontab -
+(crontab -l ; echo "*/5 * * * * sh /home/scott/NAS_drive/functions/back-up.sh")| crontab -
+
+#(crontab -l ; echo "0 */48 * * * sh /home/scott/NAS_drive/functions/back-up.sh")| crontab -
