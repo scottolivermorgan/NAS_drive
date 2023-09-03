2ff36997e1959d9f9e0ae96497a4f26fe267b130
Scott Oliver Morgan <scottolivermorgan@yahoo.com>
2023-09-03 13:44:23 +0100

    echo feedback added.

--- functions/back-up.sh
+++ functions/back-up.sh
@@ -1,9 +1,17 @@
+echo "Closing airgap"
+
 python /home/scott/NAS_drive/functions/relay_power_on.py
 
+echo "Mounting hard drive"
+
 sleep 20s
+
+echo "Syncing drives"
 
 rsync -av /media/scott/cloudDrive/* /media/scott/cloudDriveBU
 
 sleep 20s
+
+echo "Closing airgap"
 
 python /home/scott/NAS_drive/functions/relay_power_off.py
