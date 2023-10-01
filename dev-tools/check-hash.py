a1a177b71918ae1aa2581a29980bc6ff94a74561
Scott Oliver Morgan <scottolivermorgan@yahoo.com>
2023-10-01 16:21:03 +0100

    outline synch protected approch.

---
+++ dev-tools/check-hash.py
@@ -0,0 +1,12 @@
+# Build dir tree of external HD
+
+# Get list of hash file names from Pi.
+
+# Varify hash files exist on external HD.
+
+# Verify Hash's match.
+
+# If yes run rsync.
+
+# If no shutdown.
+
---
+++ dev-tools/write-hash.py
@@ -0,0 +1,9 @@
+# Select random location within Pi OS filesystem.
+
+# Create dir.
+
+# Create random number of hash files limeted to 20 -100 within dir.
+
+# At random select a random number of hash files.
+
+# Copy said files to random loctions on external HD.
