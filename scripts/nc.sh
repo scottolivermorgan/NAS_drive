1045313c85d93dc1fa561c4959751ac2c3ab0797
Scott Oliver Morgan <scottolivermorgan@yahoo.com>
2023-11-18 17:41:12 +0000

    added missing php modules.

--- scripts/nc.sh
+++ scripts/nc.sh
@@ -55,7 +55,7 @@ apt update
 
 # Install relevent PHP packages
 echo "Installing required PHP packages"
-yes | sudo apt-get install libapache2-mod-php8.2 php8.2-zip php8.2-mysql php8.2-curl php8.2-xml php8.2-mbstring php8.2-gd php8.2-smbclient
+yes | sudo apt-get install libapache2-mod-php8.2 php8.2-zip php8.2-mysql php8.2-curl php8.2-xml php8.2-mbstring php8.2-gd php8.2-smbclient php8.2-gmp php8.2-bcmath php8.2-intl php8.2-imagick
 
 
 #yes | apt install libapache2-mod-php8.1 php8.1-zip php8.1-mysql php8.1-curl php8.1-xml php8.1-mbstring php8.1-gd 
