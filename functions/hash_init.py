c6eb296f4be6f3b153c73e8206ad432d1034c68b
Scott Morgan <scottolivermorgan@yahoo.com>
2023-12-31 13:13:14 +0000

    temp fix to stop recursive search over lg external HD.

--- functions/hash_init.py
+++ functions/hash_init.py
@@ -10,9 +10,12 @@ print("dst = ",DESTINATION_DIR)
 """
 locs = [select_random_location(SOURCE_DIR),
          select_random_location(DESTINATION_DIR)]
+"""
+locs = [select_random_location(SOURCE_DIR),
+         DESTINATION_DIR]
+
 hash_locations = create_hash_file(locs)
 
 with open(".env", "w", encoding="utf8") as f:
     f.write(f"SRC_VALIDATION_HASH_LOC = '{hash_locations[0]}'\n")
     f.write(f"DST_VALIDATION_HASH_LOC = '{hash_locations[1]}'")
-"""
