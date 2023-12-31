28f37e05e787872ae860c411a125c3c2273c65e0
Scott Morgan <scottolivermorgan@yahoo.com>
2023-12-31 12:08:24 +0000

    debug test

--- functions/hash_init.py
+++ functions/hash_init.py
@@ -4,6 +4,7 @@ from helpers import select_random_location, create_hash_file
 SOURCE_DIR = os.getcwd()
 DESTINATION_DIR = "/media/hardrive1"
 
+"""
 locs = [select_random_location(SOURCE_DIR),
          select_random_location(DESTINATION_DIR)]
 hash_locations = create_hash_file(locs)
@@ -11,3 +12,4 @@ hash_locations = create_hash_file(locs)
 with open(".env", "w", encoding="utf8") as f:
     f.write(f"SRC_VALIDATION_HASH_LOC = '{hash_locations[0]}'\n")
     f.write(f"DST_VALIDATION_HASH_LOC = '{hash_locations[1]}'")
+"""
