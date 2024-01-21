68432d31bc7c58e8edfd4611543e370e67ba53e0
Scott Morgan <scottolivermorgan@yahoo.com>
2024-01-21 16:41:08 +0000

    main.py now mounts drives.

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
 import json
+import subprocess
 
 fp = "config.json"
 
-# Open and read the config.json file
+# Open and read the config.json file:
 with open(fp, 'r') as config_file:
     config_data = json.load(config_file)
 
-for object in config_data:
-    print(object)
+
+for object in config_data['HD_map']:
+
+    # get drive mapping details:
+    EXTERNAL_HD = config_data["HD_map"][object]["name"]
+    back_up_drive_name = hd_name = config_data["HD_map"][object]["back_up_name"]
+    signal_pin= hd_name = config_data["HD_map"][object]["GPIO_pin"]
+
+    # Build shell cmd's to pass to subprocesses:
+
+    # Look up UUID of eternal hd and set as an environment variable
+    # Note: Commands extract full details of drive from blkid,
+    # parse for uuid, strip 'uuid=', strip leading whitespace.
+    shell_UUID_str = f"export DRIVE_UUID=$(blkid --match-token LABEL=\"${EXTERNAL_HD}\" | grep -o ' UUID=\"[^\"]*' | sed 's/UUID=\"//' | sed 's/^ *//');"
+
+    # Build mount point:
+    mount_location_str = f"mkdir /media/{EXTERNAL_HD};"
+
+    # Mount:
+    fstab_entry = f"echo \"UUID=${shell_UUID_str}    /media/hardrive1               ntfs    defaults,errors=remount-ro 0       1\" >> /etc/fstab;"
+    cmd = shell_UUID_str + mount_location_str + fstab_entry
+
+    # Execute command to mount drive in fstab:
+    subprocess.run(cmd)
--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Define the path to your config.json file
+# Define the path to config.json file
 config_file="config.json"
 
 # Check if the config file exists
@@ -9,101 +9,78 @@ if [ ! -f "$config_file" ]; then
     exit 1
 fi
 
+echo "Enter current user name?"
+read User_name
+export UN="$User_name"
 
+echo "Install Next Cloud y/n?"
+read nc_option
 
-jq -c '.[]' config.json | while read i; do
-    echo "$i"
-done
+if [ "$nc_option" = "y" ]; then
+    echo "Create nextcloud user:"
+    read nc_user
+    export NC_USER="$nc_user"
+    echo "Set nextcloud user password: "
+    read nc_password
+    export NC_PASSWORD="$nc_password"
+fi
 
-# Use jq to loop over the items in HD_map and print the "name" value
-#echo "$json" | jq '.HD_map | to_entries[] | .value.name'
+echo "Attach external Hard drive? y/n?"
+read ext_hd
 
-# Parse the JSON and count the objects within "HD_map"
-#available_drives=$(jq '.HD_map | length' "$config_file")
-#
-## Set the environment variable
-#export AVALIBLE_DRIVES="$available_drives"
-#
-## Print the result
-#echo "AVALIBLE_DRIVES is set to: $AVALIBLE_DRIVES"
-#
-## Loop over the drives
-#for i in "$AVALIBLE_DRIVES"; do
-#  echo "Processing drive: $$AVALIBLE_DRIVES"
-#  # Add your logic here for each drive
-#done
-#echo "Enter current user name?"
-#read User_name
-#export UN="$User_name"
-#
-#echo "Install Next Cloud y/n?"
-#read nc_option
-#
-#if [ "$nc_option" = "y" ]; then
-#    echo "Create nextcloud user:"
-#    read nc_user
-#    export NC_USER="$nc_user"
-#    echo "Set nextcloud user password: "
-#    read nc_password
-#    export NC_PASSWORD="$nc_password"
-#fi
-#
-#echo "Attach external Hard drive? y/n?"
-#read ext_hd
-#
 #if [ "$ext_hd" = "y" ]; then
 #    echo "Enter name of external hard drive:"
 #    read external_hd
 #    export EXTERNAL_HD="$external_hd"
 #fi
-#
-#echo "Configure air gapped backup y/n?"
-#read ag_bu
-#
-#if [ "$ag_bu" = "y" ]; then
-#    echo "somthing"
-#fi
-#
-#echo "Configure shutdown switch y/n?"
-#read shutdown_switch
-#
-#if [ "$shutdown_switch" = "y" ]; then
-#    echo "shutdown_switch"
-#fi
-#
-#echo "Install Plex server y/n?"
-#read plex
-#
-#if [ "$plex" = "y" ]; then
-#    echo "plex"
-#fi
-#
-#if [ "$nc_option" = "y" ]; then
-#    sh NAS_drive/scripts/nextcloud/nextcloud-dependancies.sh
-#    sh NAS_drive/scripts/nextcloud/nextcloud-installation.sh
-#    sh NAS_drive/scripts/nextcloud/nextcloud-setup.sh
-#fi
-#
-#if [ "$ext_hd" = "y" ]; then
-#    sh NAS_drive/scripts/nextcloud/mount-drives.sh
-#    echo "enable external storage via nextcloud GUI, type y when enabled."
-#    read dummy
-#    #sudo -u www-data php /var/www/nextcloud/occ files:scan --all --verbose
-#fi
-#
-#if [ "$ag_bu" = "y" ]; then
-#    sh NAS_drive/scripts/backup_drive/schedule-backup.sh
-#fi
-#
-#if [ "$ag_bu" = "y" ]; then
-#    sh NAS_drive/scripts/shutdown_switch/shutdown.sh
-#fi
-#
-#if [ "$plex" = "y" ]; then
-#    sh NAS_drive/scripts/plex/plex-installation.sh
-#    sh NAS_drive/scripts/plex/mv_meta_loc.sh
-#fi
-#
-#yes | sudo sh NAS_drive/scripts/harden_security/auto_patch.sh
-#
-#sudo reboot
+
+echo "Configure air gapped backup y/n?"
+read ag_bu
+
+if [ "$ag_bu" = "y" ]; then
+    echo "somthing"
+fi
+
+echo "Configure shutdown switch y/n?"
+read shutdown_switch
+
+if [ "$shutdown_switch" = "y" ]; then
+    echo "shutdown_switch"
+fi
+
+echo "Install Plex server y/n?"
+read plex
+
+if [ "$plex" = "y" ]; then
+    echo "plex"
+fi
+
+if [ "$nc_option" = "y" ]; then
+    sh NAS_drive/scripts/nextcloud/nextcloud-dependancies.sh
+    sh NAS_drive/scripts/nextcloud/nextcloud-installation.sh
+    sh NAS_drive/scripts/nextcloud/nextcloud-setup.sh
+fi
+
+if [ "$ext_hd" = "y" ]; then
+    python3 main.py
+    echo "enable external storage via nextcloud GUI, type y when enabled."
+    read dummy
+    #sudo -u www-data php /var/www/nextcloud/occ files:scan --all --verbose
+fi
+
+if [ "$ag_bu" = "y" ]; then
+    sh NAS_drive/scripts/backup_drive/schedule-backup.sh
+fi
+
+if [ "$ag_bu" = "y" ]; then
+    sh NAS_drive/scripts/shutdown_switch/shutdown.sh
+fi
+
+if [ "$plex" = "y" ]; then
+    sh NAS_drive/scripts/plex/plex-installation.sh
+    sh NAS_drive/scripts/plex/mv_meta_loc.sh
+fi
+
+yes | sudo sh NAS_drive/scripts/harden_security/auto_patch.sh
+
+sudo reboot
