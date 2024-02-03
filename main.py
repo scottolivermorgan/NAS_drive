a4fc609542d1a955604d7b65756dee897a0bdbec
Scott Oliver Morgan <scottolivermorgan@yahoo.com>
2024-02-03 14:35:42 +0000

    test pipe

--- main.py
+++ main.py
@@ -24,9 +24,9 @@ if nc_option == 'y':
     env["NC_PASSWORD"] = input("Set nextcloud user password: ")
 
     print("Installing nextcloud dependancies.")
-    subprocess.run(["sudo", "sh", "scripts/nextcloud/nextcloud-dependancies.sh"])
-    subprocess.run(["sudo", "sh", "scripts/nextcloud/nextcloud-installation.sh"], env=env)
-    subprocess.run(["sudo", "sh", "scripts/nextcloud/nextcloud-setup.sh"], env=env)
+    #subprocess.run(["sudo", "sh", "scripts/nextcloud/nextcloud-dependancies.sh"])
+    #subprocess.run(["sudo", "sh", "scripts/nextcloud/nextcloud-installation.sh"], env=env)
+    subprocess.run([ f"{env['NC_PASSWORD']}","|","sudo", "sh", "scripts/nextcloud/nextcloud-setup.sh"], env=env)
 """
 if ext_hd == 'y':
     EXTERNAL_HD, back_up_drive_name, signal_pin = mount_HD_from_config(config_data)
