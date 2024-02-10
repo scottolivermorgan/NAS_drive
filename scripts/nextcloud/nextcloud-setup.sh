c27ed99b69f73c4a6ab9ebc36f29b8ca175daa6e
Scott Oliver Morgan <scottolivermorgan@yahoo.com>
2024-02-10 13:30:00 +0000

    echoing script feedback.

--- scripts/nextcloud/nextcloud-setup.sh
+++ scripts/nextcloud/nextcloud-setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Create & configure database for nextcloud
+echo "Creating & configuring database for nextcloud"
  echo '$UN'| "CREATE DATABASE nextclouddb;
       CREATE USER '$NC_USER'@'localhost' IDENTIFIED BY '$NC_PASSWORD';
       GRANT ALL PRIVILEGES ON nextclouddb.* TO '$NC_USER'@'localhost';
