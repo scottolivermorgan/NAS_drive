d76158fa66ad1381221ea149373213cd368789fd
Scott Oliver Morgan <scottolivermorgan@yahoo.com>
2024-02-11 14:22:32 +0000

    user outputs added.

--- scripts/nextcloud/nextcloud-setup.sh
+++ scripts/nextcloud/nextcloud-setup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -o pipefail
-
 # Create & configure database for nextcloud
 echo "Creating & configuring database for nextcloud"
 echo "CREATE DATABASE nextclouddb;
@@ -9,5 +7,5 @@ echo "CREATE DATABASE nextclouddb;
      GRANT ALL PRIVILEGES ON nextclouddb.* TO '$NC_USER'@'localhost';
      FLUSH PRIVILEGES;
      Quit" | sudo mysql -u root -p
-
+echo " nextclouddb created, '$NC_USER' added with pw '$NC_PASSWORD'"
 echo "Nextcloud-setup complete, please run nextcloud-installation"
