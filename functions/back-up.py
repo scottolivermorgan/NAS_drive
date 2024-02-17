7014e8926896cfb3c79640e07540432bec4279ae
Scott Oliver Morgan <scottolivermorgan@yahoo.com>
2024-02-17 13:14:39 +0000

    house keeping

--- functions/back-up.py
+++ functions/back-up.py
@@ -1,50 +1,14 @@
 import json
-from helpers import power_on
-import time
-import subprocess
-import RPi.GPIO as GPIO
 import os
-
-def backup_HD(config_data):
-    
-    print("Closing airgap")
-
-    for object in config_data['HD_map']:
-        # get drive mapping details:
-        EXTERNAL_HD = config_data["HD_map"][object]["name"]
-        back_up_drive_name = hd_name = config_data["HD_map"][object]["back_up_name"]
-        signal_pin= hd_name = config_data["HD_map"][object]["GPIO_pin"]
-        
-        print(f"Mounting {back_up_drive_name} hard drive")
-        power_on(signal_pin, ON=True)
-        time.sleep(20)
-        print(f"Syncing {back_up_drive_name} drive with {EXTERNAL_HD}")
-
-        #rsync_cmd = f"rsync -av --log-file=\"/home/{os.getenv('USER')}/NAS_drive/logs/sync_log.log\" /media/{EXTERNAL_HD}/* /media/{os.getenv('USER')}/{back_up_drive_name}"
-        #print(rsync_cmd)
-        #sync = subprocess.run(rsync_cmd, shell=True, capture_output=True, text=True)
-        log_file_path = f"/home/{os.getenv('USER')}/NAS_drive/logs/sync_log.log"
-        rsync_cmd = f"sudo rsync -av --log-file='{log_file_path}' /media/{EXTERNAL_HD}/* /media/{os.getenv('USER')}/{back_up_drive_name}"
-        print(rsync_cmd)
-
-        # Open log file in append mode so that logs get appended
-        with open(log_file_path, 'a') as log_file:
-            sync = subprocess.run(rsync_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-            # Write stdout and stderr to both console and log file
-            print(sync.stdout)
-            print(sync.stderr)
-            log_file.write(sync.stdout)
-            log_file.write(sync.stderr)
-        
-        time.sleep(20)#
-        print("Closing airgap")
-        power_on(signal_pin, ON=False)
+from helpers import backup_HD
 
 if __name__ == "__main__":
-    fp = "/home/scott/NAS_drive/config.json" # change user
+    # Path to hard drives config file.
+    fp = f"/home/{os.getenv('USER')}/NAS_drive/config.json"
 
     # Open and read the config.json file:
     with open(fp, 'r') as config_file:
         config_data = json.load(config_file)
-        
+    
+    # Sync all drives to designated air gapped backups.
     backup_HD(config_data)
--- functions/helpers.py
+++ functions/helpers.py
@@ -7,6 +7,7 @@ import random
 import os
 from dotenv import load_dotenv
 import re
+import time
 
 def power_on(RELAY_CHANNEL, ON) -> None:
     """
@@ -313,3 +314,39 @@ def hash_init(config_data):
             f.write(f"{EXTERNAL_HD}_SRC_VALIDATION_HASH_LOC = '{hash_locations[0]}'\n")
             f.write(f"{EXTERNAL_HD}_DST_VALIDATION_HASH_LOC = '{hash_locations[1]}'")
         print("hash checks written")
+
+
+def backup_HD(config_data):
+    
+    print("Closing airgap")
+
+    for object in config_data['HD_map']:
+        # get drive mapping details:
+        EXTERNAL_HD = config_data["HD_map"][object]["name"]
+        back_up_drive_name = hd_name = config_data["HD_map"][object]["back_up_name"]
+        signal_pin= hd_name = config_data["HD_map"][object]["GPIO_pin"]
+        
+        print(f"Mounting {back_up_drive_name} hard drive")
+        power_on(signal_pin, ON=True)
+        time.sleep(20)
+        print(f"Syncing {back_up_drive_name} drive with {EXTERNAL_HD}")
+
+        #rsync_cmd = f"rsync -av --log-file=\"/home/{os.getenv('USER')}/NAS_drive/logs/sync_log.log\" /media/{EXTERNAL_HD}/* /media/{os.getenv('USER')}/{back_up_drive_name}"
+        #print(rsync_cmd)
+        #sync = subprocess.run(rsync_cmd, shell=True, capture_output=True, text=True)
+        log_file_path = f"/home/{os.getenv('USER')}/NAS_drive/logs/sync_log.log"
+        rsync_cmd = f"sudo rsync -av --log-file='{log_file_path}' /media/{EXTERNAL_HD}/* /media/{os.getenv('USER')}/{back_up_drive_name}"
+        print(rsync_cmd)
+
+        # Open log file in append mode so that logs get appended
+        with open(log_file_path, 'a') as log_file:
+            sync = subprocess.run(rsync_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+            # Write stdout and stderr to both console and log file
+            print(sync.stdout)
+            print(sync.stderr)
+            log_file.write(sync.stdout)
+            log_file.write(sync.stderr)
+        
+        time.sleep(20)
+        print("Closing airgap")
+        power_on(signal_pin, ON=False)
