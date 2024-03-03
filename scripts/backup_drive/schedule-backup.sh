a01ac9e4951ff8ddcbb4abcf88fed9a89479b444
Scott Oliver Morgan <scottolivermorgan@yahoo.com>
2024-03-03 19:14:25 +0000

    add env vars to cron.

--- scripts/backup_drive/schedule-backup.sh
+++ scripts/backup_drive/schedule-backup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # Write scheduled hard drive backup job to cron.
+(crontab -l ; echo "USER=$UN")| crontab -
 (crontab -l ; echo "0 5 * * * python /home/$UN/NAS_drive/functions/back-up.py >> /home/$UN/NAS_drive/logs/back_up-cron.log 2>&1")| crontab -
