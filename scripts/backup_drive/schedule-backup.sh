af56235f2566154616c5c44824e5c3fb2e642dfb
Scott Oliver Morgan <scottolivermorgan@yahoo.com>
2024-03-10 15:29:08 +0000

    fp rename.

--- scripts/backup_drive/schedule-backup.sh
+++ scripts/backup_drive/schedule-backup.sh
@@ -2,4 +2,4 @@
 
 # Write scheduled hard drive backup job to cron.
 (crontab -l ; echo "USER=$UN")| crontab -
-(crontab -l ; echo "0 5 * * * python /home/$UN/NAS_drive/functions/back-up.py >> /home/$UN/NAS_drive/logs/back_up-cron.log 2>&1")| crontab -
+(crontab -l ; echo "0 5 * * * python /home/$UN/NAS_drive/functions/back-up.py >> /home/$UN/NAS_drive/logs/back_up_cron.log 2>&1")| crontab -
