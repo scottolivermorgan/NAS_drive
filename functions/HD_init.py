710de6471de6fb19be48cee117de3ac9b14dad16
Scott Oliver Morgan <scottolivermorgan@yahoo.com>
2024-03-21 18:25:08 +0000

    fixed relative import issue.

--- functions/HD_init.py
+++ functions/HD_init.py
@@ -1,19 +1,20 @@
 import json
 import os
-from lib.helpers import mount_HD_from_config, hash_init
+from library.helpers import mount_HD_from_config, hash_init
 
-# Get the directory of the current script
-current_dir = os.path.dirname(os.path.abspath(__file__))
-    
-# Navigate to the config directory
-config_dir = os.path.join(current_dir, '..', 'config')
-    
-# Open the config.json file
-config_path = os.path.join(config_dir, 'config.json')
+if __name__ == "__main__":
+    # Get the directory of the current script
+    current_dir = os.path.dirname(os.path.abspath(__file__))
 
-# Open and read the config.json file:
-with open(config_path, "r") as config_file:
-    config_data = json.load(config_file)
+    # Navigate to the config directory
+    config_dir = os.path.join(current_dir, '..', 'config')
 
-drive_mapping = mount_HD_from_config(config_data)
-initalise_hashes = hash_init(config_data)
+    # Open the config.json file
+    config_path = os.path.join(config_dir, 'config.json')
+
+    # Open and read the config.json file:
+    with open(config_path, "r") as config_file:
+        config_data = json.load(config_file)
+
+    drive_mapping = mount_HD_from_config(config_data)
+    initalise_hashes = hash_init(config_data)
