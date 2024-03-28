11f4037dabdb10f6c00d345694c2f5ebc9cbb935
Scott Oliver Morgan <scottolivermorgan@yahoo.com>
2024-03-28 16:58:27 +0000

    jelyfin metadata backup routine

--- library/jellyfin_helpers.py
+++ library/jellyfin_helpers.py
@@ -1,27 +1,50 @@
 import subprocess
 
 # Function to sync local Jellyfin metadata to external hard drive.
+
+#TODO mkdir FIRST if dosent exist
 def sync_directories():
-    # Define source and destination directories
-    directories = [
-        ("/mnt/jellyfin/cache/", "/media/HD_1/Media/metadata/jellyfin/cache/"),
-        ("/mnt/jellyfin/config/", "/media/HD_1/Media/metadata/jellyfin/config/")
+    source_dir = "/mnt/jellyfin"
+    destination_dir = "/media/HD_1/Media/metadata/jellyfin"
+
+    # Construct rsync command
+    rsync_command = [
+        "rsync",
+        "-av",  # Options for archive mode and verbose output
+        "--delete",  # Delete extraneous files from destination directories
+        source_dir,
+        destination_dir
     ]
 
-    # Iterate over directories and perform rsync
-    for src, dest in directories:
-        rsync_command = [
-            "rsync",
-            "-avz",  # Options for rsync: archive mode, verbose, compress
-            "--delete",  # Delete extraneous files from dest dirs
-            src,
-            dest
-        ]
-        try:
-            subprocess.run(rsync_command, check=True)
-            print(f"Synced {src} to {dest} successfully.")
-        except subprocess.CalledProcessError as e:
-            print(f"Error syncing {src} to {dest}: {e}")
+    try:
+        # Execute rsync command
+        subprocess.run(rsync_command, check=True)
+        print("Directories synced successfully!")
+    except subprocess.CalledProcessError as e:
+        print(f"Error syncing directories: {e}")
 
-sync_directories()
+
+
+#TODO mkdir FIRST if dosent exist
 # Function to restore Jellyfin data from external hard drive to local.
+def restore_metadata():
+    source_directory = "/media/HD_1/Media/metadata/jellyfin"
+    destination_directory = "/mnt/jellyfin"
+
+    # Construct the rsync command
+    rsync_command = [
+        "rsync",
+        "-avz",  # Options: archive, verbose, compress
+        source_directory,
+        destination_directory
+    ]
+
+    try:
+        # Execute the rsync command using subprocess
+        subprocess.run(rsync_command, check=True)
+        print("Directories synced successfully!")
+    except subprocess.CalledProcessError as e:
+        print(f"Error syncing directories: {e}")
+
+#sync_directories()
+#restore_metadata()
