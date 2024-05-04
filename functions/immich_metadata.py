b721b30b04d277a4e4551f7a22756f21405cf81e
scottolivermorgan <scottolivermorgan@yahoo.com>
2024-05-04 16:12:11 +0100

    miich metadata sync function added

---
+++ functions/immich_metadata.py
@@ -0,0 +1,10 @@
+import sys
+import os
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
+
+from library.helpers import sync_immich_directories
+
+if __name__ == "__main__":
+    var = sync_immich_directories()
