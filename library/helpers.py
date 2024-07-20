cf6411aee472ff5b17e3631070f022220b1e6244
scottolivermorgan <scottolivermorgan@yahoo.com>
2024-07-20 17:39:33 +0100

    rm komga meta sync->superceeded

--- library/helpers.py
+++ library/helpers.py
@@ -441,52 +441,6 @@ def backup_HD(config_data):
             print("Closing airgap")
             power_on(signal_pin, ON=False)
 
-def sync_komga_directories():
-    source_dir = "/mnt/komga"
-    destination_dir = "/media/HD_1/Media/metadata/"
-
-    # Construct rsync command
-    rsync_command = [
-        "rsync",
-        "-av",  # Options for archive mode and verbose output
-        "--delete",  # Delete extraneous files from destination directories
-        source_dir,
-        destination_dir,
-    ]
-
-    try:
-        # Execute rsync command
-        subprocess.run(rsync_command, check=True)
-        print("Directories synced successfully!")
-    except subprocess.CalledProcessError as e:
-        print(f"Error syncing directories: {e}")
-
-
-# TODO mkdir FIRST if dosent exist
-# Function to restore Jellyfin data from external hard drive to local.
-def restore_komga_metadata():
-    source_directory = "/media/HD_1/Media/metadata/komga"
-    destination_directory = "/mnt/"
-
-    # Construct the rsync command
-    rsync_command = [
-        "rsync",
-        "-avz",  # Options: archive, verbose, compress
-        source_directory,
-        destination_directory,
-    ]
-
-    try:
-        # Execute the rsync command using subprocess
-        subprocess.run(rsync_command, check=True)
-        print("Directories synced successfully!")
-    except subprocess.CalledProcessError as e:
-        print(f"Error syncing directories: {e}")
-
-
-# rsync -avz /home/{{ ansible_user }}/NAS_drive/ansible/tasks/immich_compose/postgres/ /media/HD_1/Media/metadata/immich/postgres/
-
-
 def sync_immich_directories():
     source_dir = "/home/scott/NAS_drive/ansible/tasks/immich_compose/postgres/"
     destination_dir = "/media/HD_1/Media/metadata/immich/postgres"
