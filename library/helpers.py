3c8217fce1542e84367c5a928f87ecb775597a2a
scottolivermorgan <scottolivermorgan@yahoo.com>
2024-07-20 17:36:13 +0100

    rm jellyfin metadata sync ->superceeded

--- library/helpers.py
+++ library/helpers.py
@@ -441,59 +441,6 @@ def backup_HD(config_data):
             print("Closing airgap")
             power_on(signal_pin, ON=False)
 
-
-import subprocess
-
-# Function to sync local Jellyfin metadata to external hard drive.
-
-
-# TODO mkdir FIRST if dosent exist
-def sync_jellyfin_directories():
-    source_dir = "/mnt/jellyfin"
-    destination_dir = "/media/HD_1/Media/metadata/"
-
-    # Construct rsync command
-    rsync_command = [
-        "rsync",
-        "-av",  # Options for archive mode and verbose output
-        "--delete",  # Delete extraneous files from destination directories
-        source_dir,
-        destination_dir,
-    ]
-
-    try:
-        # Execute rsync command
-        subprocess.run(rsync_command, check=True)
-        print("Directories synced successfully!")
-    except subprocess.CalledProcessError as e:
-        print(f"Error syncing directories: {e}")
-
-
-# TODO mkdir FIRST if dosent exist
-# Function to restore Jellyfin data from external hard drive to local.
-def restore_jellyfin_metadata():
-    source_directory = "/media/HD_1/Media/metadata/jellyfin"
-    destination_directory = "/mnt/"
-
-    # Construct the rsync command
-    rsync_command = [
-        "rsync",
-        "-avz",  # Options: archive, verbose, compress
-        source_directory,
-        destination_directory,
-    ]
-
-    try:
-        # Execute the rsync command using subprocess
-        subprocess.run(rsync_command, check=True)
-        print("Directories synced successfully!")
-    except subprocess.CalledProcessError as e:
-        print(f"Error syncing directories: {e}")
-
-
-import subprocess
-
-
 def sync_komga_directories():
     source_dir = "/mnt/komga"
     destination_dir = "/media/HD_1/Media/metadata/"
