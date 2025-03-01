a1ab0abe7cca758a2cc25ede40b9d1fa0c59eef4
Scott Morgan <scottolivermorgan@yahoo.com>
2025-03-01 13:49:44 +0000

    incl media names in notification msg

--- functions/notify_new_media.py
+++ functions/notify_new_media.py
@@ -2,13 +2,17 @@ import subprocess
 from library.helpers import get_files_created_today
 
 if __name__ == "__main__":
-    fp = ['/media/HD_1/Media/TV Shows',
-    '/media/HD_1/Media/Movies']
+    fp = ['/media/HD_1/Media/TV Shows', '/media/HD_1/Media/Movies']
 
     for folder in fp:
         updated, new_files = get_files_created_today(folder)
 
         if updated:
-            command = ['curl', '-d', 'New Media availible', 'http://192.168.1.9:8090/media_updated']
+            # Create the message string with the list of new files
+            new_files_message = 'New Media available: ' + ', '.join(new_files)
+
+            # Prepare the command with the updated message
+            command = ['curl', '-d', new_files_message, 'http://192.168.1.9:8090/media_updated']
+            
+            # Make the API call
             ntfy_call = subprocess.run(command, capture_output=True)
-        
