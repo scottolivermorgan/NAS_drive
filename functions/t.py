d049d69bbf4bd4223589b815fcade46c6562e3dd
Scott Morgan <scottolivermorgan@yahoo.com>
2025-03-09 13:29:14 +0000

    test

--- functions/t.py
+++ functions/t.py
@@ -1,3 +1,4 @@
+import time
 import RPi.GPIO as GPIO
 
 
@@ -6,4 +7,11 @@ RELAY_CHANNEL = 23
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(RELAY_CHANNEL, GPIO.OUT)
-GPIO.output(RELAY_CHANNEL, GPIO.HIGH)
+
+
+# Flash the LED 10 times
+for _ in range(10):
+    GPIO.output(RELAY_CHANNEL, GPIO.HIGH)
+    time.sleep(0.25)
+    GPIO.output(RELAY_CHANNEL, GPIO.LOW)
+    time.sleep(0.25)
