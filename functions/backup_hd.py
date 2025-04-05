eb2a2d2a0a2188149735de99ade13d9aaab85dd6
Scott Morgan <scottolivermorgan@yahoo.com>
2025-04-05 12:56:34 +0100

    bla

--- functions/backup_hd.py
+++ functions/backup_hd.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import subprocess
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
@@ -11,6 +12,20 @@ from library.helpers import (
     stop_all_docker_containers,
     execute_rsync
     )
+
+def run_bash_script(script_path):
+    """Function to run the bash script."""
+    try:
+        # Execute the bash script using subprocess
+        result = subprocess.run(['bash', script_path], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        # If successful, return 0 (success code)
+        print(result.stdout.decode())  # Optionally print the output of the script
+        return 0
+    except subprocess.CalledProcessError as e:
+        # If there's an error, print the error and return non-zero code
+        print(f"Error executing script: {e.stderr.decode()}")
+        return 1
+
 
 if __name__ == "__main__":
 
@@ -29,7 +44,9 @@ if __name__ == "__main__":
         config["backup_volume_group"],
         config["backup_logical_volumes"][0]["name"]
         )
-    docker_stop = stop_all_docker_containers()
+    
+    script_path = './stop_docker_containers.sh'  # Path to your bash script
+    docker_stop = run_bash_script(script_path)
     
     if docker_stop == 0:
         execute_rsync()
